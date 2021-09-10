a1373d2fefaabd3b5e476dc26577c18463d821cf
DESKTOP-FFDF7RM\Albi <albikrasniqi31@gmail.com>
2021-09-10 13:53:26 +0200

    notifications

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { json } = require('body-parser')
+const FCM = require('fcm-node')
+
+
 require('dotenv/config')
 
 const app = express()
@@ -10,8 +13,11 @@ app.use(json())
 
 // Import routes
 const userRoute = require('./routes/users')
+const notificationRoute = require('./routes/notifications')
+
 
 app.use('/users', userRoute)
+app.use('/notifications', notificationRoute)
 
 
 // Connect to DB
@@ -24,6 +30,23 @@ mongoose.connect(
 }, () => console.log('conetect to db')
 )
 
+// fcm end point
+// app.post('/fcm', async(req, res, next) => {
+//   try{
+//     let fcm = new FCM(SERVER_KEY)
+//     let message = {
+//       to: '/deviceId/'  + req.body.deviceId,
+//       notification : {
+//         title : req.body.title,
+//         body : req.body.body
+//       }
+//     }
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+
 
 // Routes
 app.get('/', (req, res) => {
@@ -33,3 +56,4 @@ app.get('/', (req, res) => {
 
 // Listening
 app.listen(4000)
+
--- routes/notifications.js
+++ routes/notifications.js
@@ -4,20 +4,31 @@ const Notification = require('../models/Notification')
 
 const router = express.Router()
 
+// Get All
+router.get('/notification', function(req, res) {
+    Notification.find(function(err, data) {
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.send(data)
+        }
+    })
+ })
 
 // Find by:Id
-router.get('/:notificationId', async (req, res) => {
-    try{
-        const post = await User.findById(req.params.userId)
-        res.json(post)
-    } catch (err) {
-        res.json({message: err})
-    }
-})
+// router.get('/:notificationId', async (req, res) => {
+//     try{
+//         const post = await Notification.findById(req.params.notificationId)
+//         res.json(post)
+//     } catch (err) {
+//         res.json({message: err})
+//     }
+// })
 
 
-router.post('/notification' , (req, res) => {
-    const post = new User({
+router.post('/' , (req, res) => {
+    const post = new Notification({
         userId: req.body.userId,
         head: req.body.head,
         body: req.body.body
